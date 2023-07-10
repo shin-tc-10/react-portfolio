16eaea6537b971c38416d4984108c265f793f52e
shin-tc-10 <shin.nkt.09@gmail.com>
2023-07-10 20:43:48 +0900

    スキル一覧作成

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,23 +8,115 @@ function Main() {
           <div className="top-img"><img src="../images/shin-top-img.jpg" alt="top-image" /></div>
         </section>
         <section>
-          <div className="Works" id="Works">
+          <div className="works" id="works">
             <h2>Works</h2>
-            <div>Todoアプリ</div>
+            <div className="works-app-box">
+                <div className="works-app">
+                    <p>ブログアプリ</p>
+                    <div className="works-img">
+                        <img src="../images/works1.png" alt="works-image" />
+                    </div>
+                    <p>
+                        このアプリはサンプルです。このアプリはサンプルです。
+                        <br/>このアプリはサンプルです。このアプリはサンプルです。
+                    </p>
+                </div>
+                <div className="works-app">
+                    <p>SNSアプリ</p>
+                    <div className="works-img">
+                        <img src="../images/works1.png" alt="works-image" />
+                    </div>
+                    <p>
+                        このアプリはサンプルです。このアプリはサンプルです。
+                        <br/>このアプリはサンプルです。このアプリはサンプルです。
+                    </p>
+                </div>
+                <div className="works-app">
+                    <p>Todoアプリ</p>
+                    <div className="works-img">
+                        <img src="../images/works1.png" alt="works-image" />
+                    </div>
+                    <p>
+                        このアプリはサンプルです。このアプリはサンプルです。
+                        <br/>このアプリはサンプルです。このアプリはサンプルです。
+                    </p>
+                </div>
+            </div>
           </div>
         </section>
         <section>
-          <div className="Skills" id="Skills">
+          <div className="skills" id="skills">
             <h2>Skills</h2>
+            <div className="skills-box">
+                <div className="skill">
+                    <p>HTML</p>
+                    <div className="top-img">
+                        <img src="../images/html.svg" alt="works-image" />
+                    </div>
+                    <p>実務経験2年</p>
+                </div>
+                <div className="skill">
+                    <p>CSS</p>
+                    <div className="top-img">
+                        <img src="../images/css.svg" alt="works-image" />
+                    </div>
+                    <p>実務経験2年</p>
+                </div>
+                <div className="skill">
+                    <p>JavaScript</p>
+                    <div className="top-img">
+                        <img src="../images/javascript.svg" alt="works-image" />
+                    </div>
+                    <p>実務経験2年</p>
+                </div>
+                <div className="skill">
+                    <p>jQuery</p>
+                    <div className="top-img">
+                        <img src="../images/jquery.svg" alt="works-image" />
+                    </div>
+                    <p>実務経験1年</p>
+                </div>
+            </div>
+            <div className="skills-box">
+                <div className="skill">
+                    <p>React</p>
+                    <div className="top-img">
+                        <img src="../images/react.svg" alt="works-image" />
+                    </div>
+                    <p>自己学習3ヶ月</p>
+                </div>
+                <div className="skill">
+                    <p>Java</p>
+                    <div className="top-img">
+                        <img src="../images/java.svg" alt="works-image" />
+                    </div>
+                    <p>実務経験2年</p>
+                </div>
+                <div className="skill">
+                    <p>SpringBoot</p>
+                    <div className="top-img">
+                        <img src="../images/spring.svg" alt="works-image" />
+                    </div>
+                    <p>実務経験1年</p>
+                </div>
+                <div className="skill">
+                    <p>SQL</p>
+                    <div className="top-img">
+                        <img src="../images/oracle.svg" alt="works-image" />
+                    </div>
+                    <p>実務経験2年</p>
+                </div>
+            </div>
           </div>
         </section>
         <section>
-          <div className="AboutMe" id="AboutMe">
+          <div className="about-me" id="about-me">
             <h2>About me</h2>
+            <p>経歴</p>
           </div>
         </section>
         <section>
-          <div className="Contact" id="Contact">
+          <div className="contact" id="contact">
             <h2>Contact</h2>
           </div>
         </section>

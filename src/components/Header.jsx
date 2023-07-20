e4d3441d3b577005147abf78461106a347c45752
shin-tc-10 <shin.nkt.09@gmail.com>
2023-07-20 22:52:03 +0900

    レスポンシブ対応

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,60 +58,6 @@ function Header() {
       )}
     </header>
   );
-  // return (
-  //   <div>
-  //     <header>
-  //       <nav>
-  //         <ul className="menu">
-  //           <li className="menu-list">
-  //             <Link
-  //               to="works"
-  //               spy={true}
-  //               smooth={true}
-  //               offset={50}
-  //               duration={500}
-  //             >
-  //               Works
-  //             </Link>
-  //           </li>
-  //           <li className="menu-list">
-  //             <Link
-  //               to="skills"
-  //               spy={true}
-  //               smooth={true}
-  //               offset={50}
-  //               duration={500}
-  //             >
-  //               Skills
-  //             </Link>
-  //           </li>
-  //           <li className="menu-list">
-  //             <Link
-  //               to="about"
-  //               spy={true}
-  //               smooth={true}
-  //               offset={50}
-  //               duration={500}
-  //             >
-  //               About
-  //             </Link>
-  //           </li>
-  //           <li className="menu-list">
-  //             <Link
-  //               to="link"
-  //               spy={true}
-  //               smooth={true}
-  //               offset={50}
-  //               duration={500}
-  //             >
-  //               Blog / GitHub
-  //             </Link>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default Header;

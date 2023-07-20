2cd842e41d35e0b3fffc9825ccbcdbeaf2241e00
shin-tc-10 <shin.nkt.09@gmail.com>
2023-07-20 23:26:21 +0900

    レスポンシブ対応

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,50 +12,30 @@ function Header() {
     <header className="header">
       {/* <div className="logo">Your Logo</div> */}
       {/* メディアクエリでスマートフォンの幅以下の場合に表示 */}
-      {window.innerWidth <= 768 && (
-        <>
-          <div
-            className={`hamburger ${isMenuOpen ? "open" : ""}`}
-            onClick={toggleMenu}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <nav className={`menu ${isMenuOpen ? "open" : ""}`}>
-            <Link
-              to="works"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-            >
-              Works
-            </Link>
-            <Link
-              to="skills"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-            >
-              Skills
-            </Link>
-            <Link
-              to="about"
-              spy={true}
-              smooth={true}
-              offset={50}
-              duration={500}
-            >
-              About
-            </Link>
-            <Link to="link" spy={true} smooth={true} offset={50} duration={500}>
-              Blog / GitHub
-            </Link>
-          </nav>
-        </>
-      )}
+      <>
+        <div
+          className={`hamburger ${isMenuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <nav className={`menu ${isMenuOpen ? "open" : ""}`}>
+          <Link to="works" spy={true} smooth={true} offset={50} duration={500}>
+            Works
+          </Link>
+          <Link to="skills" spy={true} smooth={true} offset={50} duration={500}>
+            Skills
+          </Link>
+          <Link to="about" spy={true} smooth={true} offset={50} duration={500}>
+            About
+          </Link>
+          <Link to="link" spy={true} smooth={true} offset={50} duration={500}>
+            Blog / GitHub
+          </Link>
+        </nav>
+      </>
     </header>
   );
 }
